7aef80958215eb0b42445bfeb197a17b9469b349
Nour elhouda Ghodbane <ghn2610a@ensfsi.onmicrosoft.com>
2024-04-24 11:24:47 +0200

    Initialisation

---
+++ src/fizz/FizzBuzz.java
@@ -0,0 +1,5 @@
+package fizz;
+
+public class FizzBuzz {
+
+}
---
+++ test/fizz/FizzTest.java
@@ -0,0 +1,14 @@
+package fizz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
